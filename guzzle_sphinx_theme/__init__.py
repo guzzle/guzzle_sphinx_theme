a136004bf69fd8bdf56c2e37b5617443e9a97653
Michael Dowling <mtdowling@gmail.com>
2013-09-19 11:23:09 -0700

    PEP 8

--- guzzle_sphinx_theme/__init__.py
+++ guzzle_sphinx_theme/__init__.py
@@ -18,17 +18,20 @@ def setup(app):
     app.connect('build-finished', create_sitemap)
     app.sitemap_links = []
 
+
 def add_html_link(app, pagename, templatename, context, doctree):
     """As each page is built, collect page names for the sitemap"""
-    base_url = app.config["html_theme_options"]["base_url"]
+    base_url = app.config['html_theme_options'].get('base_url', '')
     if base_url:
         app.sitemap_links.append(base_url + pagename + ".html")
 
+
 def create_sitemap(app, exception):
     """Generates the sitemap.xml from the collected HTML page links"""
-    if app.config["html_theme_options"]["base_url"] == "": return
-    if exception is not None: return
-    if len(app.sitemap_links) == 0: return
+    if (not app.config['html_theme_options'].get('base_url', '') or
+           exception is not None or
+           not app.sitemap_links):
+        return
 
     filename = app.outdir + "/sitemap.xml"
     print "Generating sitemap.xml in %s" % filename
@@ -38,8 +41,7 @@ def create_sitemap(app, exception):
 
     for link in app.sitemap_links:
         url = ET.SubElement(root, "url")
-        loc = ET.SubElement(url, "loc")
-        loc.text = link
+        ET.SubElement(url, "loc").text = link
 
     ET.ElementTree(root).write(filename)
 
