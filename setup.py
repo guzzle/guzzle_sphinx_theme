dede52df35ca09797370bde23dfae5e50c38b066
Michael Dowling <mtdowling@gmail.com>
2013-09-19 15:40:10 -0700

    Fixing css breaking

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='guzzle_sphinx_theme',
-    version='0.3',
+    version='0.3.1',
     description='Sphinx theme used by Guzzle.',
     long_description=open('README.rst').read(),
     author='Michael Dowling',
