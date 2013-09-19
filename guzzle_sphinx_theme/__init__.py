c1ad8729e960dc61fde42fa577da0d2e00c95be2
Michael Dowling <mtdowling@gmail.com>
2013-09-19 10:48:22 -0700

    Adding the ability to automatically generate a sitemap.xml from html files

--- guzzle_sphinx_theme/__init__.py
+++ guzzle_sphinx_theme/__init__.py
@@ -1,7 +1,7 @@
 """Sphinx Guzzle theme."""
 
 import os
-import inspect
+import xml.etree.ElementTree as ET
 
 from docutils import nodes
 from sphinx.locale import admonitionlabels
@@ -10,6 +10,38 @@ from sphinx.writers.html import HTMLTranslator as SphinxHTMLTranslator
 from pygments.style import Style
 from pygments.token import Keyword, Name, Comment, String, Error, \
      Number, Operator, Generic, Whitespace, Punctuation, Other, Literal
+
+
+def setup(app):
+    """Setup conntects events to the sitemap builder"""
+    app.connect('html-page-context', add_html_link)
+    app.connect('build-finished', create_sitemap)
+    app.sitemap_links = []
+
+def add_html_link(app, pagename, templatename, context, doctree):
+    """As each page is built, collect page names for the sitemap"""
+    base_url = app.config["html_theme_options"]["base_url"]
+    if base_url:
+        app.sitemap_links.append(base_url + pagename + ".html")
+
+def create_sitemap(app, exception):
+    """Generates the sitemap.xml from the collected HTML page links"""
+    if app.config["html_theme_options"]["base_url"] == "": return
+    if exception is not None: return
+    if len(app.sitemap_links) == 0: return
+
+    filename = app.outdir + "/sitemap.xml"
+    print "Generating sitemap.xml in %s" % filename
+
+    root = ET.Element("urlset")
+    root.set("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9")
+
+    for link in app.sitemap_links:
+        url = ET.SubElement(root, "url")
+        loc = ET.SubElement(url, "loc")
+        loc.text = link
+
+    ET.ElementTree(root).write(filename)
 
 
 def html_theme_path():
