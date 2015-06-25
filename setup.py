2e3dabd73fdeda7e11661cf648695c259d3e932d
Michael Dowling <mtdowling@gmail.com>
2015-06-24 17:54:51 -0700

    0.7.9 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='guzzle_sphinx_theme',
-    version='0.7.8',
+    version='0.7.9',
     description='Sphinx theme used by Guzzle.',
     long_description=open('README.rst').read(),
     author='Michael Dowling',
